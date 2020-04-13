050ee489f2ef1cbf31291c4a32f64dee7202cb2c
Sanjar Adylov <sshadylov@gmail.com>
2020-04-13 22:48:43 +0500

    Modify Stage1 query: include only small molecules.

--- data/stage1_smiles.sql
+++ data/stage1_smiles.sql
@@ -5,38 +5,39 @@
   as they will be analyzed in rediscovery studies in Stage 2.
 */
 
+WITH target_ids AS (
+  SELECT tid
+  FROM target_dictionary
+  WHERE
+        pref_name LIKE 'Plasmodium falciparum%'
+    AND pref_name LIKE 'Staphylococcus aureus%'
+    AND pref_name IN (
+      'Serotonin 2a (5-HT2a) receptor',
+      '5-hydroxytryptamine receptor 2A',
+      'Serotonin receptor 2a and 2c (5HT2A and 5HT2C)',
+      'Serotonin receptor 2a and 2b (5HT2A and 5HT2B)',
+      'Serotonin 2 receptors; 5-HT2a & 5-HT2c',
+      'Dopamine D2 receptor and Serotonin 2a receptor (D2 and 5HT2a)'
+    )
+), assay_ids AS (
+  SELECT assay_id
+  FROM assays
+  WHERE tid NOT IN target_ids
+), molregnos AS (
+  SELECT DISTINCT molregno
+  FROM activities
+  WHERE
+    assay_id IN assay_ids
+    AND standard_type IN ('IC50', 'MIC')
+    AND standard_relation IS NOT NULL
+
+  INTERSECT
+
+  SELECT molregno
+  FROM molecule_dictionary
+  WHERE molecule_type = 'Small molecule'
+)
+
 SELECT DISTINCT canonical_smiles
 FROM compound_structures
-  JOIN activities
-    ON compound_structures.molregno = activities.molregno
-WHERE
-  activities.assay_id IN (
-
-    SELECT assay_id
-    FROM assays
-    WHERE
-      tid IN (
-
-        SELECT tid
-        FROM target_dictionary
-        WHERE
-          -- Here we ignore not only the targets of interest but also the targets of
-          -- akin target names.
-          -- ??? Should we exclude only exact matches, i.e.
-          --     target.dictionary.pref_name != 'Plasmodium falciparum'?
-              pref_name NOT LIKE 'Plasmodium falciparum%'
-          AND pref_name NOT LIKE 'Staphylococcus aureus%'
-          AND pref_name NOT IN (
-            'Serotonin 2a (5-HT2a) receptor',
-            '5-hydroxytryptamine receptor 2A',
-            'Serotonin receptor 2a and 2c (5HT2A and 5HT2C)',
-            'Serotonin receptor 2a and 2b (5HT2A and 5HT2B)',
-            'Serotonin 2 receptors; 5-HT2a & 5-HT2c',
-            'Dopamine D2 receptor and Serotonin 2a receptor (D2 and 5HT2a)'
-          )
-    )
-
-  )
-
-  AND activities.standard_type IN ('IC50', 'MIC')
-  AND activities.standard_relation IS NOT NULL;
+WHERE molregno in molregnos;
