4853106f5c81bf2e51e068ca47db50cb5343d8b0
Sanjar Adylov <sshadylov@gmail.com>
2020-05-22 18:42:31 +0500

    Add setup.py (no tests performed).

---
+++ setup.py
@@ -0,0 +1,29 @@
+"""
+Set up moleculegen-ml package.
+"""
+
+from setuptools import setup
+from urllib.parse import urljoin
+
+
+PACKAGE_URL = 'http://github.com/sanjaradylov/moleculegen-ml/'
+
+
+setup(
+    name='moleculegen',
+    description='Generate novel molecules using recurrent neural networks',
+    version='1.0.dev',
+    author='Sanjar Ad[iy]lov',
+    url=PACKAGE_URL,
+    project_urls={
+        'Documentation': urljoin(PACKAGE_URL, 'wiki'),
+        'Source Code': urljoin(PACKAGE_URL, 'tree/master/moleculegen'),
+    },
+    packages=[
+        'moleculegen',
+    ],
+    include_package_data=False,
+    install_requires=[
+        'mxnet-cu101mkl',
+    ],
+)
