b02319595388fa69fc6d47853a1cd4c9c3b7e0e6
Sanjar Adylov <16402077+sanjaradylov@users.noreply.github.com>
2021-03-04 10:42:09 +0500

    Refactor MLP.

--- moleculegen/estimation/_gluon_common.py
+++ moleculegen/estimation/_gluon_common.py
@@ -2,10 +2,12 @@
 Gluon objects and sequential models used in the subpackage.
 """
 
-from typing import Dict, Optional
+from typing import Dict, List, Optional, Union
 
 import mxnet as mx
 from mxnet import gluon
+
+from .._types import ActivationT
 
 
 # Available RNNs.
@@ -31,16 +33,33 @@ CTX_MAP: Dict[str, mx.context.Context] = {
 def mlp(
         *,
         n_layers: int,
-        n_units: int,
-        activation: str,
+        n_units: Union[int, List[int]],
+        activation: Union[ActivationT, List[ActivationT]],
         output_dim: int,
         dtype: str,
-        dropout: float,
-        prefix: str,
-        params: Optional[mx.gluon.ParameterDict],
+        dropout: Union[float, List[float]],
+        prefix: Optional[str] = 'decoder_',
+        params: Optional[mx.gluon.ParameterDict] = None,
 ) -> gluon.Block:
     """A single dense layer or an MLP built with mxnet.gluon.nn.HybridSequential.
     """
+    n_hidden_layers = n_layers - 1
+
+    if isinstance(n_units, int):
+        n_units = [n_units] * n_hidden_layers
+    elif len(n_units) != n_hidden_layers:
+        raise ValueError(f'`n_units` must contain exactly `n_layers-1` elements.')
+
+    if not isinstance(activation, list):
+        activation = [activation] * n_hidden_layers
+    elif len(activation) != n_hidden_layers:
+        raise ValueError(f'`activation` must contain exactly `n_layers-1` elements.')
+
+    if isinstance(dropout, float):
+        dropout = [dropout] * n_hidden_layers
+    elif len(dropout) != n_hidden_layers:
+        raise ValueError(f'`dropout` must contain exactly `n_layers-1` elements.')
+
     output_dense = gluon.nn.Dense(
         units=output_dim,
         dtype=dtype,
@@ -49,23 +68,19 @@ def mlp(
         params=params,
     )
 
-    if dropout <= 1e-3 and n_layers == 1:
+    if n_layers == 1:
         return output_dense
 
     net = gluon.nn.HybridSequential(prefix=prefix)
-
-    if dropout > 1e-3:
-        net.add(gluon.nn.Dropout(dropout))
-
-    for _ in range(n_layers-1):
+    for i in range(n_hidden_layers):
         net.add(gluon.nn.Dense(
-            units=n_units,
+            units=n_units[i],
+            activation=activation[i],
             dtype=dtype,
-            activation=activation,
             flatten=False,
         ))
-        if dropout > 1e-3:
-            net.add(gluon.nn.Dropout(dropout))
+        if dropout != 0.:
+            net.add(gluon.nn.Dropout(dropout[i]))
 
     net.add(output_dense)
 
